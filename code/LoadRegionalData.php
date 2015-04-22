1698a2d0ed68b872eaea43208ac902c06247bc7d
Peter Thaleikis <bitbucket@thaleikis.de>
2015-04-22 19:58:44 +1200

    fixing typo

--- code/LoadRegionalData.php
+++ code/LoadRegionalData.php
@@ -1,28 +1,28 @@
-<?php 
+<?php
 
 class LoadRegionalData extends BuildTask{
-	
+
 	static $countries_datafile = "regionaldata/data/cdh_countries_10_29_2012.txt";
 	static $subdivisions_datafile = "regionaldata/data/cdh_subdivisions_10_29_2012.txt";
-	static $delte_existing_records = false;
-	
+	static $delete_existing_records = false;
+
 	function run($request){
 		$this->loadCountries();
 		$this->loadSubdivisions();
 	}
-	
+
 	function loadCountries(){
 		$loader = new CountriesBulkLoader("Country");
-		$loader->deleteExistingRecords = self::$delte_existing_records;
+		$loader->deleteExistingRecords = self::$delete_existing_records;
 		$results = $loader->load(self::$countries_datafile);
 		echo "loaded countries.";
 	}
-	
+
 	function loadSubdivisions(){
 		$loader = new CountrySubdivisionsBulkLoader("CountrySubdivison");
-		$loader->deleteExistingRecords = self::$delte_existing_records;
+		$loader->deleteExistingRecords = self::$delete_existing_records;
 		$results = $loader->load(self::$subdivisions_datafile);
 		echo "loaded subdivisions.";
 	}
-	
+
 }
